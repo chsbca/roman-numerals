8f6afeb6e732168794d033a77b64b5af88207c65
CHRIS <chsbca999@gmail.com>
2024-01-31 14:38:01 -0700

    added romanNumeralToArabic object in romanNumerals js

--- romanNumerals.js
+++ romanNumerals.js
@@ -1,5 +1,22 @@
 function toRomanLazy(num) {
-  return "";
+  answer = ''
+
+  const romanNumeralToArabic = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+  }
+
+  
+  /**
+   * while num > 0
+   * if num - 1000 > 0, answer += 'M'
+   */
+  
 }
 
 function toRoman(num) {
